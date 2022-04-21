bbc8702ba2bbfb3f03a26b4f6a6d5b6a0900c654
Rian <godamri@gmail.com>
2022-04-21 14:53:40 +0700

    app key

--- get.sh
+++ get.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/godamri/docker-octane-starter.git ./
 rm ./get.sh
 rm -rf ./.git
-cd ./appsrc && cp .env.example .env && composer install && npm i
+cd ./appsrc && cp .env.example .env && composer install && php artisan key:generate && npm i
 cd ./..
