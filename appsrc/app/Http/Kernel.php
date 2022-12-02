c99fc6278692add8623b9ebef535c79e5d884b2b
Rian <10650472+godamri@users.noreply.github.com>
2022-12-02 13:01:29 +0700

    Update Kernel.php
    run log by default

--- appsrc/app/Http/Kernel.php
+++ appsrc/app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \App\Http\Middleware\CommunicationLog::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
