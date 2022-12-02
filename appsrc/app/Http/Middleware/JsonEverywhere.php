524ac7ac709f70df04dfdeca01e78fb6cb643d10
Rian <10650472+godamri@users.noreply.github.com>
2022-12-02 13:00:18 +0700

    Update JsonEverywhere.php
    add request_id

--- appsrc/app/Http/Middleware/JsonEverywhere.php
+++ appsrc/app/Http/Middleware/JsonEverywhere.php
@@ -8,7 +8,12 @@ class JsonEverywhere
     {
         $request->headers->set('Accept', 'application/json' );
         $request->headers->set('Content-Type', 'application/json' );
+        $request->headers->set('X-Request-Id', 'WT.' . strtoupper(sha1(time())) );
 
-        return $next($request);
+        $response = $next($request);
+
+        $response->headers->set('X-Request-Id', $request->header('X-Request-Id') );
+
+        return $response;
     }
 }
